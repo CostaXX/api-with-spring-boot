00da81a1c4ecd350ed2e5eec59fbc01da7f7ce5d
CostaXX <dulcaconstantin24@gmail.com>
2025-08-05 00:29:55 +0200

    init

---
+++ src/main/java/com/openclassrooms/api_with_spring_boot/ApiWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.api_with_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassrooms/api_with_spring_boot/ApiWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.api_with_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
